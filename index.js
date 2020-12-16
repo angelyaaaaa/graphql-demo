459a5b733e155b947e83cfcd5a68ba916cb46203
angelyaaaaa <tsoen.526018@gmail.com>
2020-12-16 21:32:42 +0800

    feat: add new field - user and fake data

--- index.js
+++ index.js
@@ -2,19 +2,54 @@ const { ApolloServer, gql } = require('apollo-server');
 // ApolloServer: class to start server, implementing graphqla and basic web application(express)
 // gql: template literal tag, able to use GraphQL syntax in js
 
+// NOTE: fake data
+const users = [
+    {
+        id: 1,
+        name: 'Fong',
+        age: 23
+    },
+    {
+        id: 2,
+        name: 'Kevin',
+        age: 40
+    },
+    {
+        id: 3,
+        name: 'Mary',
+        age: 18
+    }
+];
+
+
 // 1. GraphQL Schema
 const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
+    """
+    user info
+    """
+    type User {
+        "ID"
+        id: ID
+        "Name"
+        name: String
+        "Age"
+        age: Int
+    }
+
+    type Query {
+        "A simple type for getting started!"
+        hello: String
+        "Get current user"
+        me: User
+    }
 `;
 
 // 2. Resolvers is a function mapping field in schema, abling to processing data first then return back to gql server
 const resolvers = {
     Query: {
         // NOTE: absolutely map to field name in schema
-        hello: () => 'world'
+        hello: () => 'world',
+        me: () => users[0],
     }
 };
 
@@ -24,5 +59,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-    console.log(`? Server ready at ${url}`);
+    console.log(`=== Server ready at ${url} ===`);
 });
